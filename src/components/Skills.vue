12bbcc196ff0e22ed2c50b46a4d2302862128168
R DeGroot <robert.b.degroot@gmail.com>
2018-09-11 15:24:55 -0400

    vue -- first work on app learning vue
     - first work on app learning vue

---
+++ src/components/Skills.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+
+      <!-- this displays an input where data submitted gets added to the skills possessed list -->
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you possess..." v-model="skill" v-validate="'min:5'" name="skill">
+
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')"> {{ errors.first('skill') }} </p>
+        </transition>>
+      </form>
+
+      <!-- this displays the list of skills possessed to the UI -->
+      <ul>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>
+            {{ data.skill }}
+          </li>
+        </transition-group>
+      </ul>
+
+      <p>These are the skills that you possess:</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  'name': 'Skills',
+  data() {
+    return {
+      'skill': '',
+      'skills': [
+        { 'skill': 'Vue.js' },
+        { 'skill': 'Frontend Developer' },
+        { 'skill': 'Backend Developer' },
+        { 'skill': 'Full Stack Developer' },
+      ],
+    };
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({ 'skill': this.skill });
+          this.skill = '';
+        } else {
+          console.log('Not valid input'); // TODO: write something better for the UI to alert this to the user
+        }
+      });
+    }, // TODO: write a method to remove skills from the existing list on the UI
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css";
+
+  .holder {
+    background: #fff
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align: center;
+    padding: 30px 0;
+    color: gray
+  }
+
+  .container {
+    box-shadow: 0px 0px, 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+</style>
